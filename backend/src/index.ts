c7c912b921116459fb467b9d82dc65e5556cba01
jayakarthik-jk <karthick72002@gmail.com>
2023-04-25 18:40:45 +0530

    routes defined

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,12 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
+import brands from "./routes/brands";
+import models from "./routes/models";
+import category from "./routes/category";
+import products from "./routes/products";
+import users from "./routes/users";
+
 const app = express();
 
 app.use(cors());
@@ -9,8 +15,14 @@ app.use(helmet());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Recomm Prototype API");
 });
+
+app.use("/api/brands", brands);
+app.use("/api/models", models);
+app.use("/api/category", category);
+app.use("/api/products", products);
+app.use("/api/users", users);
 
 const PORT = process.env.PORT || 3000;
 
