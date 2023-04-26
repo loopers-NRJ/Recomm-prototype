68625bd9138a1a7de9ce14b69a117b11e680ab1d
jayakarthik-jk <karthick72002@gmail.com>
2023-04-26 11:26:41 +0530

    Implemented products route

---
+++ backend/src/database/products.ts
@@ -0,0 +1,65 @@
+import client from "./client";
+
+export const getProducts = async () => {
+  const response = await client.product.findMany();
+  return response;
+};
+
+export const getProduct = async (id: string) => {
+  const response = await client.product.findUnique({
+    where: { id },
+  });
+  return response;
+};
+
+export const getProductByUserId = async (userId: string) => {
+  const response = await client.product.findMany({
+    where: {
+      userId,
+    },
+  });
+  return response;
+};
+
+export const createProduct = async (
+  userId: string,
+  modelId: string,
+  price: number,
+  color: string,
+  images: string[]
+) => {
+  // not optimized yet
+  const user = await client.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!user) return new Error("User not found");
+
+  const model = await client.model.findUnique({
+    where: {
+      id: modelId,
+    },
+  });
+  if (!model) return new Error("Model not found");
+
+  const response = await client.product.create({
+    data: {
+      model: {
+        connect: {
+          id: model.id,
+        },
+      },
+      user: {
+        connect: {
+          id: user.id,
+        },
+      },
+      color,
+      price,
+      pictures: images,
+    },
+  });
+  return response;
+};
--- backend/src/routes/products.ts
+++ backend/src/routes/products.ts
@@ -1,5 +1,56 @@
 import { Router } from "express";
+import { createProduct, getProduct, getProducts } from "../database/products";
+
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: "public/uploads/",
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname +
+        "-" +
+        uniqueSuffix +
+        "." +
+        file.originalname.split(".").pop()
+    );
+  },
+});
+
+const upload = multer({ storage, limits: { fileSize: 1000000 } });
 
 const router = Router();
+
+router.get("/", async (req, res) => {
+  const products = await getProducts();
+  res.json(products);
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await getProduct(id);
+  res.json(product);
+});
+
+router.post("/", upload.array("pictures", 5), async (req, res) => {
+  const { userId, color, price, modelId } = req.body;
+  if (!req.files)
+    return res.status(400).json({ error: "No files were uploaded." });
+  if (!Array.isArray(req.files))
+    return res.status(400).json({ error: "Files must be an array." });
+  // remove the public/ from the path
+  const pictures = req.files.map((file) => file.path.replace("public/", ""));
+  if (pictures.length === 0)
+    return res.status(400).json({ error: "No files were uploaded." });
+
+  if (Number.isNaN(+price))
+    return res.status(400).json({ error: "Price must be a number." });
+
+  const product = await createProduct(userId, modelId, +price, color, pictures);
+  if (product instanceof Error)
+    return res.status(400).json({ error: product.message });
+  res.json(product);
+});
 
 export default router;
