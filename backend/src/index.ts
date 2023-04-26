bde2f133f50156648d72b759778d32ad08747718
jayakarthik-jk <karthick72002@gmail.com>
2023-04-26 11:35:43 +0530

    modified index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,6 @@ import cors from "cors";
 import helmet from "helmet";
 
 import brands from "./routes/brands";
-import models from "./routes/models";
 import category from "./routes/category";
 import products from "./routes/products";
 import users from "./routes/users";
@@ -14,17 +13,18 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send("Recomm Prototype API");
 });
 
 app.use("/api/brands", brands);
-app.use("/api/models", models);
 app.use("/api/category", category);
 app.use("/api/products", products);
 app.use("/api/users", users);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
