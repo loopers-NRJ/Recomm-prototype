b3f181d76f371091e423ab6547e13fdcdf1ea8e3
jayakarthik-jk <karthick72002@gmail.com>
2023-04-26 11:22:43 +0530

    Implemented brand route

--- backend/src/routes/brands.ts
+++ backend/src/routes/brands.ts
@@ -1,5 +1,26 @@
 import { Router } from "express";
+import models from "./models";
+import { createBrand, getBrand, getBrands } from "../database/brands";
 
 const router = Router();
+
+router.get("/", async (req, res) => {
+  const brands = await getBrands();
+  res.json(brands);
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const brand = await getBrand(id);
+  res.json(brand);
+});
+
+router.use("/:brandId/models", models);
+
+router.post("/", async (req, res) => {
+  const { name } = req.body;
+  const brand = await createBrand(name);
+  res.json(brand);
+});
 
 export default router;
