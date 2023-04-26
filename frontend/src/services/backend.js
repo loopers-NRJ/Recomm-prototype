feab948aa5303e96562000d00fc3db3a3a60a418
jayakarthik-jk <karthick72002@gmail.com>
2023-04-26 23:27:55 +0530

    implemented basic api utility functions

---
+++ frontend/src/services/backend.js
@@ -0,0 +1,41 @@
+import { get } from "./http";
+
+export const getCategory = async () => {
+  return get("/category");
+};
+
+export const getProducts = async () => {
+  return get("/products");
+};
+
+export const getProductsByCategory = async (categoryId) => {
+  return get(`category/${categoryId}/products`);
+};
+
+export const getBrandsByCategory = async (categoryId) => {
+  return get(`category/${categoryId}/brands`);
+};
+
+export const getBrands = async () => {
+  return get("/brands");
+};
+
+export const getProductsByBrand = async (brandId) => {
+  return get(`brands/${brandId}/products`);
+};
+
+export const getProduct = async (productId) => {
+  return get(`/products/${productId}`);
+};
+
+export const signup = async (email, name, password) => {
+  return post("/users/register", { email, name, password });
+};
+
+export const login = async (email, password) => {
+  return post("/users/login", { email, password });
+};
+
+export const getMyListings = async (userId) => {
+  return post("/users/mylistings", { userId });
+};
